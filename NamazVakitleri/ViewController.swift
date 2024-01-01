6c960051c618efb0b8946a7240f74ed5e3217ab3
Teyhan USLU <53153396+TeyUs@users.noreply.github.com>
2024-01-01 22:38:16 +0300

    init

---
+++ NamazVakitleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamazVakitleri
+//
+//  Created by Uslu, Teyhan on 1.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
